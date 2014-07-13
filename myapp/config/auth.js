b83b81697f49ff253cd5b756f3a4ef23fa50cc45
sinanm89 <sinanm89@gmail.com>
2014-07-13 21:48:32 +0300

    added core files

---
+++ myapp/config/auth.js
@@ -0,0 +1,16 @@
+/**
+ * Created by snn on 7/13/14.
+ */
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+
+    'twitterAuth' : {
+        'consumerKey' 		: 'hfE0UwYgOygXPIkICjYqjH8oy',
+        'consumerSecret' 	: 'YnRMImhXMMst02TRzgJeMNvR1KaWUC91bwuVhS1DRDf3c6EWRE',
+        'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+    }
+
+};
